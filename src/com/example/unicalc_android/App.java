6333e6ccb8eea72f4df06f6366f09db24c9bb1b8
Brian Mullan <bmullan91@gmail.com>
2014-03-07 23:44:29 +0000

    First commit

---
+++ src/com/example/unicalc_android/App.java
@@ -0,0 +1,15 @@
+package com.example.unicalc_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class App extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
