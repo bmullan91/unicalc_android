8931236d5d566c6a0fa3f8f5df4fe9470b6a417c
Brian Mullan <bmullan91@gmail.com>
2014-03-08 01:34:58 +0000

    Adding the webview object - loading with static resources found in the assest/ dir

--- src/com/example/unicalc_android/App.java
+++ src/com/example/unicalc_android/App.java
@@ -2,14 +2,29 @@ package com.example.unicalc_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class App extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //load the url
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("file:///android_asset/index.html");
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+
+
+
     }
 }
