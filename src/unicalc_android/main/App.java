008c891e073896d586a5287750d8f74ac3c0bd2d
Brian Mullan <bmullan91@gmail.com>
2014-03-11 00:21:25 +0000

    Updates - preventing zooming etc

--- src/unicalc_android/main/App.java
+++ src/unicalc_android/main/App.java
@@ -1,6 +1,7 @@
 package unicalc_android.main;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -14,13 +15,24 @@ public class App extends Activity {
 
         //load the url
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("file:///android_asset/web_app/index.html");
+
+        //chrome debugger - visit chrome://inspect/#devices
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        myWebView.loadUrl("file:///android_asset/webapp/index.html");
 
         //important!
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
 
+        //Prevent users from zooming - also prevents the webview auto zooming on input fields
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setSupportZoom(false);
+
+        //Testing localStorage first..
         //myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
 
 
